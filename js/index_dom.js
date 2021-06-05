fb7e24203c21515a1cf555baf803ef8679fa6359
Juan <juancogo38@gmail.com>
2021-06-05 02:00:29 -0500

    menu-hamburgesa

---
+++ js/index_dom.js
@@ -0,0 +1,9 @@
+import hamburgerMenu from "../js/menu_hamburgesa.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+
+hamburgerMenu(".panel-btn",".panel", ".menu a");
+
+});
