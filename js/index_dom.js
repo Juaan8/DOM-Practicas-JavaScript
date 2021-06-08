16d8ca413b033324e8d2caf904f2b9a1c577be60
Juan <juancogo38@gmail.com>
2021-06-07 23:03:08 -0500

    reloj digital

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,12 @@
 import hamburgerMenu from "../js/menu_hamburgesa.js";
+import {digitalClock, alarm} from "../js/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
 
 hamburgerMenu(".panel-btn",".panel", ".menu a");
+
+digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
 
 });
