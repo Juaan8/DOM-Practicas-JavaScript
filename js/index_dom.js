c8ab230b976f06a71ecbf6202eeafb9f85aa2bfa
Juan <juancogo38@gmail.com>
2021-06-24 14:48:13 -0500

    cuenta regresiva

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "../js/menu_hamburgesa.js";
 import {digitalClock, alarm} from "../js/reloj.js";
 import {moveBall,shortcuts} from "../js/teclado.js";
+import countdown from "../js/cuenta_regresiva.js";
 
 const d = document;
 
@@ -11,6 +12,8 @@ hamburgerMenu(".panel-btn",".panel", ".menu a");
 digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
 
 alarm("assets/alarma.mp3", "#activar-alarma","#desactivar-alarma")
+
+countdown("countdown","Jan 8,2023 05:00:00","Feliz cumpleaños");
 
 });
 
