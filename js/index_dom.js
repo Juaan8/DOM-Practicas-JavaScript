3847e6b799daeda7ef765b41b4bc4fbda4116b98
Juan <juancogo38@gmail.com>
2021-08-09 22:53:17 -0500

    Responsive Tester

--- js/index_dom.js
+++ js/index_dom.js
@@ -5,6 +5,7 @@ import countdown from "../js/cuenta_regresiva.js";
 import scrollTopButton from "../js/boton_scroll.js";
 import darkTheme from "../js/tema_oscuro.js";
 import responsiveMedia from "../js/objeto_responsive.js";
+import responsiveTester from "../js/prueba_responsive.js";
 
 const d = document;
 
@@ -39,7 +40,10 @@ responsiveMedia(
 	16869369203157!3d19.427023126223414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f
 	f35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses-419!
 	2smx!4v1626558340236!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" 
-	allowfullscreen="" loading="lazy"></iframe>`);
+	allowfullscreen="" loading="lazy"></iframe>`
+	);
+	
+	responsiveTester("responsive-tester");
 });
 
 d.addEventListener("keydown", (e) => {
